3568f84e57a05b3b2703500a189d9ab2bf4fc991
Benedict C <benedict.c@ibm.com>
2019-08-18 10:27:38 +0100

    deployment

--- src/app.py
+++ src/app.py
@@ -30,7 +30,7 @@ from knnpractice import knn_my_text_please
 #####  <<<------------------------- initial setup ------------------------------------->>>  ###########
 #####  <<<------------------------- and secret key ------------------------------------>>>  ###########
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
